c1e352290589bfa0b10cb817d9f60af0f03bd46f
nassir-zem <etudeensa@gmail.com>
2026-01-01 13:00:33 +0100

    Update MonteCarlo_reliability.py

--- src/MonteCarlo_reliability.py
+++ src/MonteCarlo_reliability.py
@@ -1,1 +1,6 @@
+import numpy as np
 
+def simulate_machine(lambda_rate, t, N=100000):
+    lifetimes = np.random.exponential(1 / lambda_rate, N)
+    reliability = np.mean(lifetimes > t)
+    return reliability
