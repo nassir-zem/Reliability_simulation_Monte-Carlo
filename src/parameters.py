ddb68f5cf1244ad759595ab7443c1db405ea8630
nassir-zem <etudeensa@gmail.com>
2026-01-01 12:55:58 +0100

    Update parameters.py

--- src/parameters.py
+++ src/parameters.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+TOTAL_TIME = 1935  # heures
+
+df = pd.read_csv("../data/downtime_data.csv")
+
+df["uptime"] = TOTAL_TIME - df["total_downtime"]
+df["MTBF"] = df["uptime"] / df["total_failures"]
+df["lambda"] = 1 / df["MTBF"]
+
+print(df)
