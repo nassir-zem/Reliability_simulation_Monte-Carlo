8bc9405a28bed95d0c98cadb94bb6a3d177dea31
nassir-zem <etudeensa@gmail.com>
2026-01-19 22:06:22 +0100

    Update parameters.py

--- src/parameters.py
+++ src/parameters.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 TOTAL_TIME = 1935  # heures
 
-df = pd.read_csv("Data/downtime_data.csv")
+df = pd.read_csv("../Data/downtime_data.csv")
 
 df["uptime"] = TOTAL_TIME - df["total_downtime"]
 df["MTBF"] = df["uptime"] / df["total_failures"]
